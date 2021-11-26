3af783937e0b759a88250d0a7d9d8d03866857ab
Riza <cak.bhot@gmail.com>
2021-11-26 10:52:18 +0700

    remove api

--- lib/constants/constant.dart
+++ lib/constants/constant.dart
@@ -1,3 +1,3 @@
-const api = "33d8ce2a4ce21ce7f12c7a18b8a1aa0d";
+const api = "";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
 const iconUrl = "https://openweathermap.org/img/wn/";
